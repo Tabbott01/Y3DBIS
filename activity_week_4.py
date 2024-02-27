3c585aa853c7d079da9eb012a3f7f8818abad4fe
Tabbott01 <94803258+Tabbott01@users.noreply.github.com>
2024-02-27 11:43:11 +0000

    Update activity_week_4.py

--- activity_week_4.py
+++ activity_week_4.py
@@ -16,8 +16,9 @@ What class do animals belong to?
 animals <- c("fox", "badger", "rabbit")
 print(class(animals))
 
-"""[1] "character"
+[1] "character"
 
+"""
 Define two vectors: numbers1 with the numbers 1 to 3 and numbers2 with the numbers 4 to 6.
 Use the c() function to combine the two vectors into a single vector named all_numbers.
 Print all_numbers to verify the combination.
@@ -28,8 +29,9 @@ numbers2 <- 4:6
 all_numbers <- c(numbers1, numbers2)
 print(all_numbers)
 
-"""[1] 1 2 3 4 5 6
+[1] 1 2 3 4 5 6
 
+"""
 Create a vector called temperatures containing the temperatures of a location for a week (e.g., 25, 28, 30, 27, 29, 26, 31).
 Use the typeof() function to determine the data type of temperatures.
 What data type is temperatures?
@@ -38,8 +40,9 @@ What data type is temperatures?
 temperatures <- c(25, 28, 30, 27, 29, 26, 31)
 print(typeof(temperatures))
 
-"""[1] "double"
+[1] "double"
 
+"""
 Suppose two more animals are added to the animals vector: "elephant" and "tiger".
 Remove the second animal from the animals vector.
 Print the modified animals vector to verify the removal.
@@ -50,5 +53,4 @@ animals <- c(animals, more_animals)
 animals <- animals[-2]
 print(animals)
 
-"""      [1] "fox"      "rabbit"   "elephant" "tiger"
-"""
+[1] "fox"      "rabbit"   "elephant" "tiger"
